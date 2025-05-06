257a05945b74c5a93d4d0fb730fb7e8e955fc6d7
Marco Braak <marco@floryn.com>
2025-05-06 21:30:17 +0200

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -1,10 +1,56 @@
-loop do
-  $stdout.write('$ ')
-  command, *args = gets.chomp.split(' ')
+class Shell
+  attr_reader :continue, :exit_code
 
-  if command == 'exit' && args.first == '0'
-    exit 0
-  else
-    puts("#{command}: command not found")
+  def initialize
+    @continue = true
+    @exit_code = 0
+  end
+
+  def run
+    read_eval_print while continue
+  end
+
+  private
+
+  def read_eval_print
+    $stdout.write('$ ')
+    command, args = read
+    return exit_repl if command.nil?
+
+    parse(command, args)
+  end
+
+  def exit_repl
+    @continue = false
+  end
+
+  def read
+    input_line = gets
+
+    if input_line.nil?
+      [nil, nil]
+    else
+      command, *args = input_line.chomp.split(' ')
+      [command, args]
+    end
+  end
+
+  def parse(command, args)
+    case command
+    when 'exit'
+      exit_repl
+    when 'echo'
+      handle_echo(args)
+    else
+      puts("#{command}: command not found")
+    end
+  end
+
+  def handle_echo(args)
+    puts(args.join(' '))
   end
 end
+
+shell = Shell.new
+shell.run
+exit(shell.exit_code)
