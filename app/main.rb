54ed8a6204a14ba8d8caebe6d5975320054d0025
Marco Braak <marco@floryn.com>
2025-05-08 08:09:02 +0200

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,4 @@
-class Shell
+class ShellContext
   attr_reader :continue, :exit_code
 
   def initialize
@@ -6,8 +6,103 @@ class Shell
     @exit_code = 0
   end
 
+  def exit_repl
+    @continue = false
+  end
+
+  def find_command_path(command)
+    paths.find do |path|
+      path.join(command).exist?
+    end
+  end
+
+  def paths
+    @paths ||= ENV['PATH'].split(':').map { Pathname.new(_1) }
+  end
+end
+
+class Command
+  attr_reader :args, :command, :context
+
+  def initialize(args:, command:, context:)
+    @args = args
+    @command = command
+    @context = context
+  end
+
+  def run; end
+end
+
+class EchoCommand < Command
   def run
-    read_eval_print while continue
+    puts(args.join(' '))
+  end
+end
+
+class ExecutableCommand < Command
+  def run
+    args_string = args.join(' ')
+    command_string = "#{command} #{args_string}"
+    system(command_string)
+  end
+end
+
+class ExitCommand < Command
+  def run
+    context.exit_repl
+  end
+end
+
+class PwdCommand < Command
+  def run
+    puts(Dir.getwd)
+  end
+end
+
+class TypeCommand < Command
+  BUILTIN_COMMANDS = %w[echo exit pwd type].freeze
+
+  def run
+    puts(builtin_result || program_result || not_found_result)
+  end
+
+  private
+
+  def command_arg
+    args[0]
+  end
+
+  def builtin?
+    BUILTIN_COMMANDS.include?(command_arg)
+  end
+
+  def command_path
+    @command_path = context.find_command_path(command_arg) unless defined? @command_path
+    @command_path
+  end
+
+  def builtin_result
+    "#{command_arg} is a shell builtin" if builtin?
+  end
+
+  def program_result
+    "#{command_arg} is #{command_path}/#{command_arg}" if command_path
+  end
+
+  def not_found_result
+    "#{command_arg}: not found"
+  end
+end
+
+class Shell
+  attr_reader :context
+
+  def initialize
+    @context = ShellContext.new
+  end
+
+  def run
+    read_eval_print while context.continue
   end
 
   private
@@ -15,13 +110,9 @@ class Shell
   def read_eval_print
     $stdout.write('$ ')
     command, args = read
-    return exit_repl if command.nil?
+    return context.exit_repl if command.nil?
 
     parse(command, args)
-  end
-
-  def exit_repl
-    @continue = false
   end
 
   def read
@@ -38,62 +129,21 @@ class Shell
   def parse(command, args)
     case command
     in 'echo'
-      handle_echo(args)
+      EchoCommand.new(args:, command:, context:).run
     in 'exit'
-      exit_repl
+      ExitCommand.new(args:, command:, context:).run
     in 'pwd'
-      handle_pwd
+      PwdCommand.new(args:, command:, context:).run
     in 'type'
-      handle_type(args)
-    in _ if find_command_path(command)
-      handle_program(command, args)
+      TypeCommand.new(args:, command:, context:).run
+    in _ if context.find_command_path(command)
+      ExecutableCommand.new(args:, command:, context:).run
     else
       puts("#{command}: command not found")
-    end
-  end
-
-  def handle_echo(args)
-    puts(args.join(' '))
-  end
-
-  def handle_type(args)
-    command = args[0]
-
-    if %w[echo exit pwd type].include?(command)
-      puts("#{command} is a shell builtin")
-      return
-    end
-
-    command_path = find_command_path(command)
-
-    if command_path
-      puts("#{command} is #{command_path}/#{command}")
-    elsif !command.nil?
-      puts("#{command}: not found")
-    end
-  end
-
-  def handle_program(command, args)
-    args_string = args.join(' ')
-    command_string = "#{command} #{args_string}"
-    system(command_string)
-  end
-
-  def handle_pwd
-    puts(Dir.getwd)
-  end
-
-  def paths
-    @paths ||= ENV['PATH'].split(':').map { Pathname.new(_1) }
-  end
-
-  def find_command_path(command)
-    paths.find do |path|
-      path.join(command).exist?
     end
   end
 end
 
 shell = Shell.new
 shell.run
-exit(shell.exit_code)
+exit(shell.context.exit_code)
