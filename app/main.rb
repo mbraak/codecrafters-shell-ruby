096ebaa592cd039f172057a1ce9fa98ca7406b0b
Marco Braak <marco@floryn.com>
2025-05-22 07:32:24 +0200

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -263,7 +263,9 @@ class RedirectOutput < StandardOutput
   def write_to_file(line)
     mode = append ? 'a' : 'w'
 
-    line = "\n#{line}" if append && !File.read(file_name).empty?
+    file_contents = File.read(file_name)
+
+    line = "\n#{line}" if append && !file_contents.empty? && !file_contents.end_with?("\n")
 
     file = File.new(file_name, mode)
     file.write(line)
